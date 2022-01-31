53bf21fef0bcc2e1f857cb1f39d05a732bbaf8d4
Ravi  Kulkarni <scmtrainer786@gmail.com>
2022-01-31 06:11:54 -0500

    updated main.tf for final solution

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,39 +1,50 @@
 provider "aws" {
-   region = var.region
+   region = "eu-central-1"
 }
 
-data "aws_availability_zones" "available" {
-  state = "available"
-}
 resource "aws_vpc" "my-vpc" {
 
-  cidr_block = var.vpc_range
+  cidr_block = "10.161.0.0/24"
 
   tags = {
     Terraform = "true"
     Environment = "dev"
   }
 }
+resource "aws_subnet" "my-subnet-a" {
 
-resource "aws_subnet" "eu_subnet" {
-
-    for_each = var.subnet_range
-    vpc_id = aws_vpc.my-vpc.id
-    cidr_block = each.value.s_cidr_range
-    availability_zone = each.value.s_availability_zone
-    tags = {
-    Name = "${each.key}"
+  vpc_id = aws_vpc.my-vpc.id
+  cidr_block = "10.161.0.0/25"
+  availability_zone = "eu-central-1a"
+  map_public_ip_on_launch = true 
+  tags = {
+    Environment = "dev"
   }
 }
+resource "aws_subnet" "my-subnet-b" {
 
-data "aws_subnet_ids" "eu_subnet_id" {
-  vpc_id = aws_vpc.my-vpc.id
 
-  depends_on = [
-    aws_subnet.eu_subnet,
-  ]
+  vpc_id     = aws_vpc.my-vpc.id
+  cidr_block = "10.161.0.128/26"
+  availability_zone = "eu-central-1b"
+  map_public_ip_on_launch = true 
+
+  tags = {
+    Environment = "dev"
+  }
 }
+resource "aws_subnet" "my-subnet-c" {
 
+
+  vpc_id     = aws_vpc.my-vpc.id
+  cidr_block = "10.161.0.192/26"
+  availability_zone = "eu-central-1c"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Environment = "dev"
+  }
+}
 resource "aws_internet_gateway" "my-IGW" {
     vpc_id = aws_vpc.my-vpc.id
 
@@ -55,31 +66,42 @@ resource "aws_route_table" "my-route-table" {
     }
 }
 
-resource "aws_route_table_association" "rta"{
-    for_each  = var.subnet_range
-    subnet_id = each.key
+resource "aws_route_table_association" "rta-a"{
+    subnet_id = aws_subnet.my-subnet-a.id
+    route_table_id = aws_route_table.my-route-table.id
+}
+
+resource "aws_route_table_association" "rta-b"{
+    subnet_id = aws_subnet.my-subnet-b.id
+    route_table_id = aws_route_table.my-route-table.id
+}
+
+resource "aws_route_table_association" "rta-c"{
+    subnet_id = aws_subnet.my-subnet-c.id
     route_table_id = aws_route_table.my-route-table.id
 }
 
 resource "aws_security_group" "generic-sg" {
     vpc_id = aws_vpc.my-vpc.id
-
+    
     egress {
         from_port = 0
         to_port = 0
         protocol = -1
         cidr_blocks = ["0.0.0.0/0"]
     }
-   dynamic "ingress" {
-    for_each = var.y_ingress
-
-      content {
-        from_port = ingress.value.port
-        to_port = ingress.value.port
-        protocol = ingress.value.protocol
-        cidr_blocks = ingress.value.y_cidr_range
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
     }
-}
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 }
 
 resource "aws_iam_role" "ec2-cloudwatch-role" {
@@ -105,18 +127,83 @@ resource "aws_iam_instance_profile" "ec2_profile" {
   role = aws_iam_role.ec2-cloudwatch-role.name
 }
 
-resource "aws_instance" "ec2-instance" {
-
-  for_each =  var.subnet_range
-  ami = var.image_id
-  instance_type = var.instance_type
-  subnet_id = each.key
+resource "aws_instance" "nginx1" {
+  
+  ami = "ami-06ec8443c2a35b0ba"
+  instance_type = "t2.micro"
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name 
+  subnet_id = aws_subnet.my-subnet-a.id
   vpc_security_group_ids = [ aws_security_group.generic-sg.id ]
-  key_name = var.key-id
- tags = { Name = "${each.key}_ec2_instance" }
+  key_name = "dev-key"
 
-  depends_on = [
-    aws_subnet.eu_subnet,
-  ]
 }
 
+resource "aws_instance" "nginx2" {
+
+  ami = "ami-06ec8443c2a35b0ba"
+  instance_type = "t2.micro"
+
+  subnet_id = aws_subnet.my-subnet-b.id
+  vpc_security_group_ids = [ aws_security_group.generic-sg.id ]
+  key_name = "dev-key"
+
+}
+resource "aws_instance" "nginx3" {
+
+  ami = "ami-06ec8443c2a35b0ba"
+  instance_type = "t2.micro"
+
+  subnet_id = aws_subnet.my-subnet-c.id
+  vpc_security_group_ids = [ aws_security_group.generic-sg.id ]
+  key_name = "dev-key"
+
+}
+
+
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
+
+  name = "my-alb"
+
+  load_balancer_type = "application"
+
+  vpc_id             = aws_vpc.my-vpc.id
+  subnets            = [aws_subnet.my-subnet-c.id, aws_subnet.my-subnet-b.id, aws_subnet.my-subnet-a.id]
+  security_groups    = [aws_security_group.generic-sg.id]
+
+
+  target_groups = [
+    {
+      name_prefix      = "pref-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+      targets = [
+        {
+          target_id = aws_instance.nginx3.id
+          port = 80
+        },
+        {
+          target_id = aws_instance.nginx2.id
+          port = 80
+        },
+        {
+          target_id = aws_instance.nginx1.id
+          port = 80
+        }
+      ]
+    }
+  ]
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+  tags = {
+    Environment = "dev"
+  }
+}
